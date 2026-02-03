10b26923db96e685c0412dc1289775ac9c5fe3d3
vika916 <15.v.romanova@cesunovads.edu.lv>
2026-02-03 09:39:48 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,12 @@
-
+#divu skaitlu pedejo ciparu summa
+print('Ievadi skaitli')
+a=int(input())
+b=int(input())
+print(a%10+b%10)
+"""
+#nauda centos, parveidot uz eiro un centiem
+print('Ievadi naudu centos')
+a=int(input())
+print(a//100)
+print(a%100)
+"""
